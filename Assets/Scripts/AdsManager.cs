f99df4cf5aee2fd34194a8c16179585e13ac55ac
Alexandre Le Mieux <alexandre.lemieux@gamepie.fr>
2016-12-18 16:22:53 +0100

    Fix commit 2

--- Assets/Scripts/AdsManager.cs
+++ Assets/Scripts/AdsManager.cs
@@ -8,8 +8,8 @@ public class AdsManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		HeyzapAds.Start("35848a20ffa97036bf2d562bcc9641ce", HeyzapAds.FLAG_DISABLE_AUTOMATIC_FETCHING);
-		HZInterstitialAd.Fetch("GameOver");
-		HZInterstitialAd.Fetch("MainMenu");
+		
+		
 
 
 	}
@@ -17,5 +17,45 @@ public class AdsManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+	}
+	
+	void FetchAdGameOver () {
+		HZInterstitialAd.Fetch("GameOver");
+	}
+	
+	void FetchAdMainMenu () {
+		HZInterstitialAd.Fetch("MainMenu");
+	}
+	
+	void ShowAdBanner () {
+		HZBannerShowOptions showOptions = new HZBannerShowOptions ();
+		showOptions.Position = HZBannerShowOptions.POSITION_BOTTOM;
+		showOptions.Tag = "Banner";
+		HZBannerAd.ShowWithOptions (showOptions);
+	}
+	
+	void DestroyAdBanner () {
+		HZBannerAd.Destroy ();
+	}
+	
+	void ShowAdMenu () {
+			if (HZInterstitialAd.IsAvailable("MainMenu")) {
+					
+				HZShowOptions showOptions = new HZShowOptions();
+				showOptions.Tag = "MainMenu";
+				HZInterstitialAd.ShowWithOptions(showOptions);
+		}
+	}
+	void ShowAdGameOver () {
+			if (HZInterstitialAd.IsAvailable("GameOver")) {
+					
+				HZShowOptions showOptions = new HZShowOptions();
+				showOptions.Tag = "GameOver";
+				HZInterstitialAd.ShowWithOptions(showOptions);
+		}
+	}
+	
+	void AdTestSuite () {
+		HeyzapAds.ShowMediationTestSuite();
 	}
 }
