457a6fea5e679db60bdffec14d7999e05aec7837
Alexandre Le Mieux <alexandre.lemieux@gamepie.fr>
2016-12-19 00:10:51 +0100

    Ads Integrated

--- Assets/Scripts/AdsManager.cs
+++ Assets/Scripts/AdsManager.cs
@@ -3,12 +3,12 @@ using System.Collections;
 using Heyzap;
 public class AdsManager : MonoBehaviour {
 
-
+public static bool showOnce = false;
 
 	// Use this for initialization
 	void Start () {
 		HeyzapAds.Start("35848a20ffa97036bf2d562bcc9641ce", HeyzapAds.FLAG_DISABLE_AUTOMATIC_FETCHING);
-		
+
 		
 
 
@@ -21,37 +21,48 @@ public class AdsManager : MonoBehaviour {
 	
 	void FetchAdGameOver () {
 		HZInterstitialAd.Fetch("GameOver");
+
 	}
 	
 	void FetchAdMainMenu () {
 		HZInterstitialAd.Fetch("MainMenu");
+
 	}
 	
 	void ShowAdBanner () {
 		HZBannerShowOptions showOptions = new HZBannerShowOptions ();
 		showOptions.Position = HZBannerShowOptions.POSITION_BOTTOM;
+		showOptions.SelectedAdMobSize = HZBannerShowOptions.AdMobSize.SMART_BANNER;
 		showOptions.Tag = "Banner";
 		HZBannerAd.ShowWithOptions (showOptions);
+
 	}
 	
 	void DestroyAdBanner () {
 		HZBannerAd.Destroy ();
+
 	}
 	
 	void ShowAdMenu () {
 			if (HZInterstitialAd.IsAvailable("MainMenu")) {
-					
-				HZShowOptions showOptions = new HZShowOptions();
-				showOptions.Tag = "MainMenu";
-				HZInterstitialAd.ShowWithOptions(showOptions);
+				if (!showOnce) {
+				//HZShowOptions showOptions = new HZShowOptions();
+				//showOptions.Tag = "MainMenu";
+				//HZInterstitialAd.ShowWithOptions(showOptions);
+				HZInterstitialAd.Show();
+
 		}
+		showOnce = true;
+			}
 	}
 	void ShowAdGameOver () {
 			if (HZInterstitialAd.IsAvailable("GameOver")) {
 					
-				HZShowOptions showOptions = new HZShowOptions();
-				showOptions.Tag = "GameOver";
-				HZInterstitialAd.ShowWithOptions(showOptions);
+				//HZShowOptions showOptions = new HZShowOptions();
+				//showOptions.Tag = "GameOver";
+				//HZInterstitialAd.ShowWithOptions(showOptions);
+				HZInterstitialAd.Show();
+
 		}
 	}
 	
